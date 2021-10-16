f06b09d753876f43c10b73c4cf215e28a4efe5f4
Alexander mokhov <wannasludge@yandex.ru>
2021-10-16 10:13:03 +0300

    trying without reading ck documentation

---
+++ src/main/java/ru/netology/domain/Comment.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Comment {
+    private int count;
+    private boolean canPost;
+    private int answer;
+    private int share;
+    private int picOfUser;
+    private int nameOfUser;
+    private int complaint;
+    private int inputField;
+    // +get/set
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int ownerId;
+    private int nameOfChannel;
+    private int threeDots;
+    private int detailsAndPic;
+    private int like;
+    private Comment comment;
+    private int share;
+    private int numberOfViews;
+    // +get/set
+}
